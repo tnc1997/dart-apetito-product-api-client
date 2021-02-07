15e95efac49dc988fae70ce62e59d9a60e14b279
Thomas Clark <tnc1997@virginmedia.com>
2021-02-07 07:39:57 +0000

    docs: update exceptions

--- lib/src/exceptions/client_exception.dart
+++ lib/src/exceptions/client_exception.dart
@@ -2,18 +2,27 @@ import 'package:apetito_product_api_client/src/extensions/response_extensions.da
 import 'package:http/http.dart';
 
 class ClientException implements Exception {
-  final String? message;
+  /// The message of the exception.
+  final String message;
+
+  /// The uri of the request if applicable.
   final Uri? uri;
+
+  /// The status code of the response if applicable.
   final int? statusCode;
+
+  /// The reason phrase of the response if applicable.
   final String? reasonPhrase;
 
+  /// Constructs a [ClientException].
   ClientException({
-    this.message,
+    required this.message,
     this.uri,
     this.statusCode,
     this.reasonPhrase,
   });
 
+  /// Constructs a [ClientException] from the [response].
   ClientException.fromResponse(Response response)
       : message = response.body,
         uri = response.request?.url,
@@ -21,9 +30,11 @@ class ClientException implements Exception {
         reasonPhrase = response.reasonPhrase;
 
   @override
-  String toString() =>
-      message != null ? 'ClientException: $message' : 'ClientException';
+  String toString() => 'ClientException: $message';
 
+  /// Checks that the [response] has a success status code.
+  ///
+  /// Throws a [ClientException] if the [response] does not have a success status code.
   static Response checkIsSuccessStatusCode(Response response) {
     if (!response.isSuccessStatusCode) {
       throw ClientException.fromResponse(response);
