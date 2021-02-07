56454ee6e8feb88bb2ab4bf290e582555180d51d
Thomas Clark <tnc1997@virginmedia.com>
2021-02-07 07:40:49 +0000

    docs: update extensions

--- lib/src/extensions/response_extensions.dart
+++ lib/src/extensions/response_extensions.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 
 extension ResponseExtensions on Response {
+  /// Returns true if the status code is success.
   bool get isSuccessStatusCode => statusCode >= 200 && statusCode < 300;
 }
